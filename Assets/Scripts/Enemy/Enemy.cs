66877f5dc0ee1d9d0c7d604d67e6ff6bdf38db63
Erica Calaça <ericazpc@gmail.com>
2024-08-21 01:30:22 -0700

    Added basic enemy movement; future update needed for player-following behavior

--- Assets/Scripts/Enemy/Enemy.cs
+++ Assets/Scripts/Enemy/Enemy.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,16 +7,58 @@ namespace FeatheredFugitive
 {
     public class Enemy : MonoBehaviour
     {
-        // Start is called before the first frame update
-        void Start()
+        public enum MovementType
         {
-
+            Left,
+            Right,
+            FollowPlayer
         }
 
-        // Update is called once per frame
-        void Update()
-        {
+        [SerializeField] private MovementType _movementType;
+        [SerializeField] private float _speed = 2f;
 
+        public float Speed
+        {
+            get { return _speed; }
+            set { _speed = value; }
         }
+
+        public MovementType CurrentMovementType
+        {
+            get { return _movementType; }
+            set { _movementType = value; }
+        }
+
+        private void Update()
+        {            
+            switch (_movementType)
+            {
+                case MovementType.Left:
+                    MoveLeft();
+                    break;
+                case MovementType.Right:
+                    MoveRight();
+                    break;
+                case MovementType.FollowPlayer:
+                    FollowPlayer();
+                    break;
+            }
+        }
+
+        private void MoveLeft()
+        {
+            transform.position += transform.right * _speed * -1 * Time.deltaTime;
+        }
+
+        private void MoveRight()
+        {
+            transform.position += transform.right * _speed * Time.deltaTime;
+        }
+
+        private void FollowPlayer()
+        {
+            Debug.Log("Create the follow player latter");
+        }
+
     }
 }
